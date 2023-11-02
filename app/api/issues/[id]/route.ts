9a736b77a46ae5fa50235a6425911a8fefbd1767
Aymen Jdily <87182808+Aymenjdily@users.noreply.github.com>
2023-11-02 21:10:41 +0100

    Improve UX

--- app/api/issues/[id]/route.ts
+++ app/api/issues/[id]/route.ts
@@ -30,3 +30,24 @@ export async function PATCH(request: NextRequest, { params }: { params: { id: st
 
     return NextResponse.json(updatedIssue, { status:201 })
 }
+
+export async function DELETE(request: NextRequest, { params }: { params: { id: string }}){
+
+    const issue = await prisma.issue.findUnique({
+        where:{
+            id: params.id
+        }
+    })
+
+    if(!issue){
+        return NextResponse.json({ error: "Invalid Issue"}, { status:404 })
+    }
+
+    const deletedIssue = await prisma.issue.delete({
+        where: {
+            id: issue.id
+        }
+    })
+
+    return NextResponse.json({}, { status:201 })
+}
